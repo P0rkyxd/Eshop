77c4be3ccd84c8a5304951921dfca56b63013c01
P0rkyxd <mates.svarc@gmail.com>
2024-04-20 21:23:21 +0200

    added trigger to show login page when you are not logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./scenes/homePage"; // Correcting the import path
 import LoginPage from "./scenes/loginPage"; // Correcting the import path
 import ProfilePage from "./scenes/profilePage"; // Correcting the import path
@@ -12,6 +12,7 @@ import { themeSettings } from "./theme"; // Assuming you have a theme settings f
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -20,8 +21,8 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+            <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
